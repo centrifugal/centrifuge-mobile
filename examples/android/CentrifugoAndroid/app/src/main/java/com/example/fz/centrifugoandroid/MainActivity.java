1b77d8299f2c2b6761b67964b6205db7941c5b1f
Alexander Emelin <frvzmb@gmail.com>
2017-02-27 23:57:50 +0300

    disconnect handler interface

--- examples/android/CentrifugoAndroid/app/src/main/java/com/example/fz/centrifugoandroid/MainActivity.java
+++ examples/android/CentrifugoAndroid/app/src/main/java/com/example/fz/centrifugoandroid/MainActivity.java
@@ -8,6 +8,7 @@ import centrifuge.Centrifuge;
 import centrifuge.Client;
 import centrifuge.Credentials;
 import centrifuge.EventHandler;
+import centrifuge.DisconnectHandler;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,13 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView tvId = (TextView) findViewById(R.id.text);
+        TextView tv = (TextView) findViewById(R.id.text);
 
         Credentials creds = Centrifuge.newCredentials(
                 "42", "1488055494", "",
                 "24d0aa4d7c679e45e151d268044723d07211c6a9465d0e35ee35303d13c5eeff"
         );
+
         EventHandler events = Centrifuge.newEventHandler();
+        DisconnectHandler disconnectHandler = new AppDisconnectHandler(this);
+        events.onDisconnect(disconnectHandler);
 
         Client client = Centrifuge.new_(
                 "ws://192.168.1.37:8000/connection/websocket",
@@ -34,9 +38,9 @@ public class MainActivity extends AppCompatActivity {
             client.connect();
         } catch (Exception e) {
             e.printStackTrace();
-            tvId.setText(e.getMessage());
+            tv.setText(e.getMessage());
             return;
         }
-        tvId.setText("Connected");
+        tv.setText("Connected");
     }
 }
